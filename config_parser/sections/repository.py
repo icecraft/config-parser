c82d07c559977422a09e7bbe3330be8201247fa9
Martin Beck <martin@riseml.com>
2017-11-07 09:49:51 +0100

    validate project name

--- config_parser/sections/repository.py
+++ config_parser/sections/repository.py
@@ -1,3 +1,7 @@
+import re
+
+from ..utils import ConfigError
+
 from .deploy import DeploySection
 from .train import TrainSection
 from .base import SectionBase
@@ -8,6 +12,8 @@ class RepositoryConfig(SectionBase):
 
     def __init__(self, project, train=None, deploy=None):
         self.project = project
+        if not re.match(r'^[A-Za-z0-9][A-Za-z0-9_\-]*[A-Za-z0-9]$', project):
+            raise ConfigError('Project name must start and end with an alphanumeric character and may additionally consist out of hyphens and underscores inbetween.')
 
         if train:
             self.train = TrainSection(**train)
